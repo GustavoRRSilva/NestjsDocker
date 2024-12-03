136db830931cb2cacd9b1d259d2400639594a68d
Gustavo <gstvrrds@gmail.com>
2024-12-03 16:51:10 -0300

    feat: adding user resources

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { EnvCnfiModule } from './shared/infrastructure/env-cnfi/env-cnfi.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [EnvCnfiModule],
+  imports: [EnvCnfiModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
