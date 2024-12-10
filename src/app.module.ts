45ff1ab8e5bac6def86c440fa817c982838f3d60
Gustavo <gstvrrds@gmail.com>
2024-12-10 19:29:36 -0300

    feat: criando testes para o metodo constructor

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
-import { EnvCnfiModule } from './shared/infrastructure/env-cnfi/env-cnfi.module';
-import { UsersModule } from './users/users.module';
+import { UsersModule } from './users/infrastructure/users.module'
 
 @Module({
-  imports: [EnvCnfiModule, UsersModule],
+  imports: [UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
